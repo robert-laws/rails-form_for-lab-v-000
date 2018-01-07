40ccdf900629471c40db0081975cff0b674e9c3f
Robert Laws <roblaws@me.com>
2018-01-07 10:02:15 +0000

    automatically backed up by learn

--- spec/models/school_class_spec.rb
+++ spec/models/school_class_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 #   before(:each) do
 #     @school_class = SchoolClass.create!(title: "Computer Science", room_number: 5)
 #   end
-# 
+#
 #   it 'can be created' do
 #     expect(@school_class).to be_valid
 #   end
-# 
+#
 # end
