a8e2352ddeaa1f5dcabcd39e50bfb6e3a411430e
Robert Laws <roblaws@me.com>
2018-01-07 09:57:33 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :students, except: [:destroy, :index]
-  resources :school_class, except: [:destroy, :index]
+  resources :school_classes, except: [:destroy, :index]
 end
